95956b0ec0ba053d0055eb12ab765249e26eefaa
OzwaldCavendish <martin.chris.wood@gmail.com>
2018-11-30 15:21:26 +0000

    Solved issue with non-flat JSON files

--- www/property_page.js
+++ www/property_page.js
@@ -1,3 +1,5 @@
+// NOTE TO SELF;  RETURNED JSON OBJECTS ARE NOT FLAT!!!
+
 // Assigns a html element to a variable for convenience
 const app = document.getElementById('root');
 
@@ -17,69 +19,55 @@ app.appendChild(logo);
 app.appendChild(container);
 
 
+// Alternative, function for making cross-site requests using CORS
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
 
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
 
-function getAllUrlParams(url) {
+  } else if (typeof XDomainRequest != "undefined") {
 
-  // get query string from url (optional) or window
-  var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
 
-  // we'll store the parameters here
-  var obj = {};
+  } else {
 
-  // if query string exists
-  if (queryString) {
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
 
-    // stuff after # is not part of query string, so get rid of it
-    queryString = queryString.split('#')[0];
-
-    // split our query string into its component parts
-    var arr = queryString.split('&');
-
-    for (var i = 0; i < arr.length; i++) {
-      // separate the keys and the values
-      var a = arr[i].split('=');
-
-      // set parameter name and value (use 'true' if empty)
-      var paramName = a[0];
-      var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
-
-      // (optional) keep case consistent
-      paramName = paramName.toLowerCase();
-      if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
-
-      // if the paramName ends with square brackets, e.g. colors[] or colors[2]
-      if (paramName.match(/\[(\d+)?\]$/)) {
-
-        // create key if it doesn't exist
-        var key = paramName.replace(/\[(\d+)?\]/, '');
-        if (!obj[key]) obj[key] = [];
-
-        // if it's an indexed array e.g. colors[2]
-        if (paramName.match(/\[\d+\]$/)) {
-          // get the index value and add the entry at the appropriate position
-          var index = /\[(\d+)\]/.exec(paramName)[1];
-          obj[key][index] = paramValue;
-        } else {
-          // otherwise add the value to the end of the array
-          obj[key].push(paramValue);
-        }
-      } else {
-        // we're dealing with a string
-        if (!obj[paramName]) {
-          // if it doesn't exist, create property
-          obj[paramName] = paramValue;
-        } else if (obj[paramName] && typeof obj[paramName] === 'string'){
-          // if property does exist and it's a string, convert it to an array
-          obj[paramName] = [obj[paramName]];
-          obj[paramName].push(paramValue);
-        } else {
-          // otherwise add the property
-          obj[paramName].push(paramValue);
-        }
-      }
-    }
   }
-
-  return obj;
+  return xhr;
 }
+
+// This url needs to change dependent on the url by which this page is called
+url = 'http://127.0.0.1:8080/properties/1'
+
+var request = createCORSRequest('GET', url);
+if (!request) {
+  throw new Error('CORS not supported');
+}
+
+request.onload = function () {
+	// Begin accessing JSON data here
+	var data = JSON.parse(this.response);
+	
+		// Check response code!
+	if (request.status >= 200 && request.status < 400) {
+		
+		// Log each property's title
+		console.log(data);
+		//console.log(data.title);
+		//console.log(data.description);
+		
+	} else {
+		console.log('error');
+	}
+};
+
+// Send request
+request.send();
